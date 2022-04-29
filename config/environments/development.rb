91470efd8078b165fdd46eef1f66b104f7c6670a
biry23 <ito.r.okr@gmail.com>
2022-04-29 09:07:43 +0000

    [add]サーバー立ち上げ完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "2fea3b8188a0443488f1e5923679e9a0.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
