09f831bf4b42b6a9d3eec4f1320462ea7b91855e
biry23 <ito.r.okr@gmail.com>
2022-04-30 06:59:24 +0000

    [add]ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
+  # public
   devise_for :members,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  scope module: 'public' do
+    root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
+    get "/members/my_page" => "members#show", as: "members"
+    get "/members/edit" => "members#edit", as: "members_edit"
+    patch "/members" => "members#update", as: "members_update"
+    resources :posts do
+      collection do
+        get "top"
+      end
+      resources :favorites, only: [:create, :destroy]
+    end
+    resources :comments, except: [:new, :edit, :update]
+  end
+  
+  # admin
+  namespace :admin do
+    root to: "homes#top"
+    resources :members, only: [:show, :index]
+    resources :posts, only: [:show, :index]
+    resources :genres, except: [:destroy, :show, :new]
+  end
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
