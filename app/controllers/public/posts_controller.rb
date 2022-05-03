6aaff01eb664ba16d5875f2312fe78529529f59a
biry23 <ito.r.okr@gmail.com>
2022-05-03 05:49:44 +0000

    [add]adminジャンル一覧・登録画面機能作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,14 +1,33 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_member!, only: [:new, :edit]
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.member_id = current_member.id
+    if @post.save!
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
+    @post =Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
