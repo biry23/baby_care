9830f5b5cdb075fbfab45fc141fc8de16e60b2d5
biry23 <ito.r.okr@gmail.com>
2022-05-03 02:36:28 +0000

    [add]ユーザー情報編集画面機能作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -9,6 +9,18 @@ class Public::MembersController < ApplicationController
   end
 
   def edit
-    @member = Member.find(params[:id])
+    @member = current_member
+  end
+
+  def update
+    @member = current_member
+    if @member.update(member_params)
+      redirect_to member_path(@member.id)
+    end
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:email, :member_name, :age, :introduction)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  before_action :authenticate_member!
   belongs_to :member
   belongs_to :pos
 end
