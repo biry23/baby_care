882e28cb550eecc28b5bbb798c5008060738c678
biry23 <ito.r.okr@gmail.com>
2022-05-11 07:28:18 +0000

    [add]Bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,12 +2,15 @@ const { environment } = require('@rails/webpacker')
 
 // 以下追記
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
 )
 // ここまで
+
 
 module.exports = environment
