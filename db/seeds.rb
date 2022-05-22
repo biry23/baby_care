98d3c0266b5c9f80299b0188fa12391a442c7a60
biry23 <ito.r.okr@gmail.com>
2022-05-22 10:53:10 +0000

    [Fix] adminログイン再度

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'ADMIN_EMAIL',
-  password: 'ADMIN_PASSWORD'
+  email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD']
 )
