acd8a80a2cb5342fa17611ef94e447204fd05432
biry23 <ito.r.okr@gmail.com>
2022-05-22 12:21:34 +0000

    [Fix] genre削除,css

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy  # 記事を削除するとお気に入りも削除
   belongs_to :member
-  belongs_to :genre, optional: true
 
   validates :title, presence: true
   validates :text, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_054731) do
+ActiveRecord::Schema.define(version: 2022_04_30_054718) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,12 +39,6 @@ ActiveRecord::Schema.define(version: 2022_04_30_054731) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["member_id"], name: "index_favorites_on_member_id"
     t.index ["post_id"], name: "index_favorites_on_post_id"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "genre_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "members", force: :cascade do |t|
